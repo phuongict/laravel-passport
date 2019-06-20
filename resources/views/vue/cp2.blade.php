3a6d485dc0c3857a7aadfd8bc123c1d0fa6c0628
Pham Phuong <phamphuong.svit@gmail.com>
2019-06-20 11:59:08 +0700

    commit v1

---
+++ resources/views/vue/cp2.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <example-component-two></example-component-two>
+        </div>
+    </div>
+</div>
+@endsection
